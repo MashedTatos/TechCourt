8caa8aa76cfffa18bb57789385dffa5bb135925d
MashedTatos <32725020+MashedTatos@users.noreply.github.com>
2018-12-14 14:55:00 -0500

    Full eclipse project

---
+++ TechCourt/TechCourt/TechCourt/src/TechCourt/DBUtil.java
@@ -0,0 +1,41 @@
+package TechCourt;
+import java.sql.*;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServletRequest;
+public class DBUtil {
+
+	
+	private Connection conn = null;
+	
+	public Connection getConnection(HttpServletRequest request) {
+		try {
+			
+			String username = request.getSession().getServletContext().getInitParameter("SQLUsername");
+			String password = request.getSession().getServletContext().getInitParameter("SQLPassword");
+			String database = request.getSession().getServletContext().getInitParameter("SQLDatabase");
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(database, username, password);
+			
+		}
+		catch(ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+	public void closeConnection(Connection conn) {
+		try {
+			conn.close();
+		} 
+		
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
+
