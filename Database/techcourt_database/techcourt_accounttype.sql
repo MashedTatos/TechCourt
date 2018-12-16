12342c20c067dff740d78ca896e02d763e082340
MashedTatos <nathanglapp@gmail.com>
2018-12-16 00:26:35 -0500

    Data dumps

--- Database/techcourt_database/techcourt_accounttype.sql
+++ Database/techcourt_database/techcourt_accounttype.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `techcourt` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `techcourt`;
 -- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
 -- Host: localhost    Database: techcourt
@@ -16,20 +14,6 @@ USE `techcourt`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `accounttype`
---
-
-DROP TABLE IF EXISTS `accounttype`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `accounttype` (
-  `TypeID` int(11) NOT NULL AUTO_INCREMENT,
-  `TypeName` varchar(20) NOT NULL,
-  PRIMARY KEY (`TypeID`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `accounttype`
@@ -50,4 +34,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-15 13:53:57
+-- Dump completed on 2018-12-16  0:24:30
